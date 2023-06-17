3da49b4b5d59fde3f7c8b2ba6eabfa21cb9347de
overlord of cum <88568085+NavaShield@users.noreply.github.com>
2023-06-17 19:45:18 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Get all elements with the specified classes
-const elements = document.querySelectorAll('.messageContent-2t3eCI, .channelName-3KPsGw, .name-3Uvkvr, .username-vAneIW, .channel-3prF2u, .membersGroup-2eiWxl, .username-3_PJ5r, .activityText-1rR-8O, .title-17SveM, .subtext-2HDqJ7, .title-338goq, .overflow-1wOqNV, .name-28HaxV, .botText-1fD6Qk, .defaultColor-1EVLSt, .item-3mHhwr, .username-Qpc78p, .text-MPIeXO, .activityText-ev7Z1T, .searchBarComponent-3N7dCG, .name-2m3Cms, .textContent-TsKzji, .title-x4dI75, .headerText-1qIDDT, .liveShapeRound-2Tgi2d, .subtext-14b69p, .text-3S7XCz, .username-h_Y3Us ');
+const elements = document.querySelectorAll('.messageContent-2t3eCI, .channelName-3KPsGw, .name-3Uvkvr, .username-vAneIW, .channel-3prF2u, .membersGroup-2eiWxl, .username-3_PJ5r, .activityText-1rR-8O, .title-17SveM, .subtext-2HDqJ7, .title-338goq, .overflow-1wOqNV, .name-28HaxV, .botText-1fD6Qk, .defaultColor-1EVLSt, .item-3mHhwr, .username-Qpc78p, .text-MPIeXO, .activityText-ev7Z1T, .searchBarComponent-3N7dCG, .name-2m3Cms, .textContent-TsKzji, .title-x4dI75, .headerText-1qIDDT, .liveShapeRound-2Tgi2d, .subtext-14b69p, .text-3S7XCz, .username-h_Y3Us, .topic-11NuQZ, .modeDefault-3Warim, .goalText-mbRUAP, .text-xs-normal-3O7EaX, .label-3CEiKJ');
 
 // Set the replacement text
 const replacementText = 'wake up';
